7716eee5944f66bbb1fc3d8a29836c260d3dce20
Nabanita Majumder <nab07.09@gmail.com>
2026-06-05 20:07:45 +0530

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        
+        //create the graph
+        //compute the indegrees
+        //select the nodes with indegree=1
+        //iterate the edges and return the edge with indegree > 0
+        int n = edges.length;
+        List<List<Integer>> adj = new ArrayList<>(n+1);
+        Queue<Integer> q = new LinkedList<>();
+        int[] indegree = new int[n+1];
+
+        for(int i=0; i<=n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges){
+            int u = edge[0]; int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+            indegree[v]++;
+            indegree[u]++;
+        }
+
+        for(int i=1; i<=edges.length; i++){
+            if(indegree[i] == 1){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            indegree[node]--;
+
+            for(int i : adj.get(node)){
+                indegree[i]--;
+                if(indegree[i]==1){
+                    q.add(i);
+                }
+            }
+        }
+
+        for(int i=edges.length-1; i>=0; i--){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            if(indegree[u] > 0 && indegree[v] > 0){
+                return new int[]{u,v};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
