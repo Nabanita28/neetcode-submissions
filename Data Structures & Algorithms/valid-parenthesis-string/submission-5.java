b1991ee889df90415b0572189dc6a30969be2792
Nabanita Majumder <nab07.09@gmail.com>
2026-06-22 14:37:25 +0530

    Add: valid-parenthesis-string - submission-5

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-5.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        
+        int low = 0;
+        int high = 0;
+
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                low++;
+                high++;
+            } else if(s.charAt(i) == ')') {
+                low--;
+                high--;
+            } else {
+                low--;
+                high++;;
+            }
+
+            if(low < 0) {
+            low = 0;
+        }
+        System.out.print("high = " + high);
+
+        //")(" is an invalid string. We need to check the order. A ')' that appears before any '(' or '*' can never be matched
+        if(high < 0){
+            return false;
+        }
+        }
+        
+        
+        return low == 0;
+        
+    }
+}
